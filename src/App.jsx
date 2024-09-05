882919fa354fe7367f454363f9e8f8c6483439c4
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-09-05 14:32:07 -0400

    basic scoreboard logic working

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
-import './styles/App.css'
+import './styles/App.css';
+
+import { useState } from 'react';
+
+import ScoreBoard from './components/ScoreBoard';
 
 function App() {
+  const [score, setScore] = useState(0);
+  const [highscore, setHighScore] = useState(0);
+
+  if (score > highscore) {
+    setHighScore(score);
+  }
+
+  function handleSetScore() {
+    const newScore = score + 1;
+    setScore(newScore);
+  }
+
+  function resetScore() {
+    setScore(0);
+  }
+
   return (
     <>
-      
+      <ScoreBoard score={score} highScore={highscore} />
+      <button onClick={handleSetScore}>add score</button>
+      <button onClick={resetScore}>reset score</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
